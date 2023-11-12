20acf0400e7cc8668902363ec7039e40a0dd3229
AlexisPinzoon <alexis.bueno@utp.edu.co>
2023-11-12 18:37:34 -0500

    Edit view Admin/Master

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,16 @@
 @extends('admin.master')
+
+@section('tittle','Dashboard')
+
+@section('content')
+<div class="container-fluid">
+    <div class="panel shadow">
+        <div class="header">
+            <h2 class="tittle"><i class="fas fa-home"></i> Inicio </h2>
+        </div>
+        <div class="inside">
+            Holaaaaaaaaaaaaaaaaaaaa
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('tittle')</title>
+    <title>@yield('tittle') - TrapStore </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="routeName" content="{{ Route::currentRouteName() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -26,7 +26,54 @@
 
         <div class="wrapper">
             <div class="col1">@include('admin.sidebar')</div>
-            <div class="col2"></div>
+            <div class="col2">
+                <nav class="navbar navbar-expand-lg shadow">
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="{{url('/admin')}}" class="nav-link">
+                                 <i class="fas fa-home"></i>Inicio</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+
+                <div class="page">
+
+                    <div class="container-fluid">
+                        <nav aria-label= "breadcrumb shadow">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="{{url('/admin')}}"><i class="fas fa-home"></i>Inicio</a>
+                                </li>
+                                @section('breadcrumb')
+                                @show
+                            </ol>
+                        </nav>
+                    </div>
+                    @if(Session::has('message'))
+                        <div class="container">
+                        <div class="alert alert-{{ Session::get('typealert')}}" style="display:none;">
+                            {{ Session::get('message')}}
+                            @if ($errors->any())
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
+                            <script>
+                                $('.alert').slideDown();
+                                setTimeout(function(){ $('.alert').slideUp();}, 50000);
+                            </script>
+                        </div>
+                        </div>
+                    @endif
+                    @section('content')
+                    @show
+                </div>
+            </div>
         </div>
 
 </body>
