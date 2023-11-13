b74699e1bc4d26a89f37cccc0a2c59ab6da3a516
AlexisPinzoon <alexis.bueno@utp.edu.co>
2023-11-12 19:44:08 -0500

    Administrate users list

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/admin')->group(function() {
     Route::get('/', 'App\Http\Controllers\Admin\DashboardController@getDashboard');
+    Route::get('/users', 'App\Http\Controllers\Admin\UserController@getUsers');
  });
